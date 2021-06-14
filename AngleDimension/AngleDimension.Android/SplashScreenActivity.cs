c6319b96df7080f7cb12448cd93239f3e24d38d1
Christopher Phiri <christopher.phiri@angledimension.com>
2021-06-14 11:34:41 +0200

    Build finalisation

---
+++ AngleDimension/AngleDimension.Android/SplashScreenActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AngleDimension.Droid
+{
+    [Activity(Label = "Angle Dimension", Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(this, typeof(MainActivity)));
+        }
+    }
+}
